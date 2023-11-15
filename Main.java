f11609a0a683ad0011a5f4e4bda47382109be1b1
MiniMenu <38903793+MiniMenu@users.noreply.github.com>
2023-11-15 09:15:23 +0530

    Thread safe invocation

--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        Singleton.getInstance().showMessage();
+        // System.out.println("---------------- invoke Singleton class  ----------------");
+        // Singleton.getInstance().showMessage();
+
+        System.out.println("---------------- Thread Safe Singleton ----------------");
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LockFreeSingleton.getInstance();
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LockFreeSingleton.getInstance();
+            }
+        });
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LockFreeSingleton.getInstance();
+            }
+        });
+        Thread thread4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                LockFreeSingleton.getInstance();
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
     }
 }
